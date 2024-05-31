987649cb95c9eaeba4837ad031fb43bef691c5c3
Gabriel <victorgabriel1730@gmail.com>
2024-05-31 11:59:33 -0300

    feat: modifies in the datatype column

--- src/controllers/CreateImmobileController.ts
+++ src/controllers/CreateImmobileController.ts
@@ -1,15 +1,13 @@
 import { Request, Response } from "express";
 import { CreateImmobileService } from "../services/CreateImmobileService";
 import { IAddress, IImmobile } from "../interfaces/interfaces";
-import { Address, Immobile } from "@prisma/client";
-
-
+//import { Address, Immobile } from "@prisma/client";
 
 
 export class CreateImmobileController {
     async handle(req: Request, res: Response) {
-        const address: Address = req.body
-        const immobile: Immobile = req.body
+        const address = req.body
+        const immobile = req.body
         const service = new CreateImmobileService();
 
         const result = service.execute(address, immobile);
