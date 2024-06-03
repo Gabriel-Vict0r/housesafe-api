955990814d9c0760d5013ab468cdaf9408cc64a0
Gabriel <victorgabriel1730@gmail.com>
2024-06-03 17:25:53 -0300

    feat: jwt token generate

--- src/controllers/LoginAdminController.ts
+++ src/controllers/LoginAdminController.ts
@@ -1,7 +1,30 @@
-
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export class LoginAdminController {
-    async handle() {
+    async handle(req: Request, res: Response) {
+        const { email, password } = req.body;
 
+        const prisma = new PrismaClient();
+
+        const admin = await prisma.admin.findFirst({
+            where: {
+                email: email
+            }
+        });
+        if (admin instanceof Error) {
+            return res.status(400).send('O e-mail informado é inválido')
+        }
+        const verifyPass = await bcrypt.compare(password, admin!.password);
+
+        if (!verifyPass) {
+            return res.status(400).send('A senha informada é inválida.')
+        }
+
+        const token = jwt.sign({ id: admin!.id }, process.env.JWT_PASS ?? '', {
+            expiresIn: '8h'
+        })
     }
 }
