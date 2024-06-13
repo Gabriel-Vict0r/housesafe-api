642b52f5ef76b2f95b44dcd393baba8536983e66
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2024-06-13 02:57:09 -0300

    fix: fix the dataype

--- src/controllers/CreateImmobileController.ts
+++ src/controllers/CreateImmobileController.ts
@@ -8,6 +8,10 @@ export class CreateImmobileController {
     async handle(req: Request, res: Response, next: NextFunction) {
         let { address } = req.body
         let { immobile } = req.body
+        if (typeof immobile !== 'object') {
+            address = JSON.parse(address)
+            immobile = JSON.parse(immobile)
+        }
         const service = new CreateImmobileService();
 
         const result = await service.execute(address, immobile);
